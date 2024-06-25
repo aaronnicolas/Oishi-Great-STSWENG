e2e4de55684b3f883e4e116bd9b32ac2addbf4fb
LanceLabarrete <97266542+LanceLabarrete@users.noreply.github.com>
2024-06-25 11:09:43 +0800

    Add files via upload

---
+++ script/location.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxL4ZgKC5zYijdg7JtwFKKX70APJDowMReSatlGQE3eF-ruxJ8Ema62fn2nB09bSsRG2g/exec'
+
+const form = document.forms['inquiry-form']
+
+form.addEventListener('submit', e => {
+   e.preventDefault()
+   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+   .then(response => alert("Thank you! Your inquiry was submitted successfully." ))
+   .then(() => { window.location.reload(); })
+   .catch(error => console.error('Error!', error.message))
+})
